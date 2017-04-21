65d83dc7fa6f4c772a32c289937df4cf25ec32da
pratekmag <prateekmag@cybage.com>
2017-04-22 01:23:03 +0530

    IPL > Home > Today's Match

---
+++ navigation/navbar.php
@@ -0,0 +1,9 @@
+<nav>
+  <ul class="navbar">
+    <li class="dektop_nav"><a href="<?php echo FOLDER_PATH_HTTP.'matches.php'?>">Matches</a></li>
+    <li class="dektop_nav"><a href="<?php echo FOLDER_PATH_HTTP.'gallery.php'?>">Voting</a></li>
+    <li class="dektop_nav"><a href="<?php echo FOLDER_PATH_HTTP.'friends.php'?>">Contribution</a></li>
+    <li class="dektop_nav"><a href="<?php echo FOLDER_PATH_HTTP.'friends.php'?>">Teams</a></li>
+    <li class="dektop_nav"><a href="<?php echo FOLDER_PATH_HTTP.'friends.php'?>">My Profile</a></li>
+  </ul>
+</nav>
